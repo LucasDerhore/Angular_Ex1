84e211807cf06ba59d7dab3a1f43ad4924d39be1
Lucas <haaji@macbook-pro-de-lucas.home>
2022-04-30 20:58:44 +0200

    test SSH key

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AppareilService } from '../services/appareil.services';
-
+//Doesn't work
 @Component({
   selector: 'app-appareil',
   templateUrl: './appareil.component.html',
